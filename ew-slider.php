97a1c0acaabfae6cd1dc2794d04db537eec9ee41
Veiko-PC\Veiko <veiko.teekel@gmail.com>
2017-07-14 20:44:06 +0300

    ew-slider file
    Starting wp plugin file

---
+++ ew-slider.php
@@ -0,0 +1,11 @@
+<?php
+   /*
+   Plugin Name: EW slider
+   Plugin URI: http://elegantweb.eu
+   Description: Slaider mis kasutab owl-carouseli ja ACF PRO-d
+   Version: 1.0
+   Author: Veiko Teekel
+   Author URI: http://elegantweb.eu
+   License: GPL2
+   */
+?>
