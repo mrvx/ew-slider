b66757eaf1823735a7f98066f9bbcb17fd406078
Veiko-PC\Veiko <veiko.teekel@gmail.com>
2017-07-14 21:24:27 +0300

    back to old ver

--- ew-slider.php
+++ ew-slider.php
@@ -3,7 +3,7 @@
    Plugin Name: EW slider
    Plugin URI: http://elegantweb.eu
    Description: Slaider mis kasutab owl-carouseli ja ACF PRO-d
-   Version: 1.0
+   Version: 2.1.4
    Author: Veiko Teekel
    Author URI: http://elegantweb.eu
    License: GPL2
