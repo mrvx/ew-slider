3ac701dcfc17b192622117ff70dd736337cd0ab0
Veiko-PC\Veiko <veiko.teekel@gmail.com>
2017-07-14 22:51:02 +0300

    custom posttype

--- ew-slider.php
+++ ew-slider.php
@@ -8,4 +8,43 @@
    Author URI: http://elegantweb.eu
    License: GPL2
    */
+
+
+
+   function ew_slider() {
+  register_post_type( 'Slaidid',
+                     array(
+                       'labels' => array(
+                         'name' => __( 'Slaidid' ),
+                         'singular_name' => __( 'slaid' )
+                       ),
+                       'public' => true,
+                       'taxonomies' => array('post_tag'),
+                       'menu_position' => 5,
+                       'rewrite' => array('slug' => 'slaid'),
+                       'supports' => array( 'title', 'editor', 'custom-fields', 'thumbnail' )
+                     )
+                    );
+}
+
+add_action( 'init', 'ew_slider' );
+
+
+function slaidirubriik_taxonomy() {
+  register_taxonomy(
+    'slaidirubriik',
+    'slaidid',
+    array(
+      'hierarchical' => true,
+      'label' => 'Slaidi rubriik',
+      'query_var' => true,
+      'orderby' => 'date',
+      'rewrite' => array('slug' => 'slaidi-rubriik')
+    )
+  );
+}
+
+add_action( 'init', 'slaidirubriik_taxonomy' );
+
+
 ?>
