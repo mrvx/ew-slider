8f082a3a36be93ae9da0b373005563808cabffb5
Veiko-PC\Veiko <veiko.teekel@gmail.com>
2017-07-15 18:27:58 +0300

    Teksti pildile lisamise võimalus

--- acf-fields.php
+++ acf-fields.php
@@ -4,7 +4,7 @@ if( function_exists('acf_add_local_field_group') ):
 
 acf_add_local_field_group(array (
 	'key' => 'group_5947e7a25e583',
-	'title' => '[:et]Slaider[:]',
+	'title' => '[:en]EW slider[:et]Slaider[:]',
 	'fields' => array (
 		array (
 			'key' => 'field_5947e7ae7c6d2',
@@ -29,6 +29,25 @@ acf_add_local_field_group(array (
 			'max_height' => '',
 			'max_size' => '',
 			'mime_types' => '',
+		),
+		array (
+			'key' => 'field_596a333c0d30b',
+			'label' => 'Tekst pildil',
+			'name' => 'tekst_pildil',
+			'type' => 'qtranslate_wysiwyg',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array (
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'tabs' => 'all',
+			'toolbar' => 'basic',
+			'media_upload' => 1,
+			'delay' => 0,
 		),
 	),
 	'location' => array (
@@ -67,5 +86,4 @@ acf_add_local_field_group(array (
 ));
 
 endif;
-
 ?>
